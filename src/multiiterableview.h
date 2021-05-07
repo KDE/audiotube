3f5b4b03f7d9e3212eb812e57b4ff92fe5ecc062
Jonah Brüchert <jbb@kaidan.im>
2021-05-07 12:54:12 +0200

    Improve MultiIterableView

--- src/multiiterableview.h
+++ src/multiiterableview.h
@@ -9,20 +9,29 @@
 #include <vector>
 #include <QtGlobal>
 
+// MultiIterableView is usually used on a small number of vectors.
+// The number is already known, so we can convert this into an if else if else etc tree
+// instead of recursively calling the function
+#ifdef __GNUC__
+#define REMOVE_RECURSION __attribute__((always_inline))
+#else
+#define REMOVE_RECURSION
+#endif
+
 template <typename... Arguments>
 class MultiIterableView {
 private:
     template<typename T, size_t i, size_t len>
-    inline size_t count(const T &tuple) const {
+    REMOVE_RECURSION inline size_t count(const T &tuple, int initial_size = 0) const {
         if constexpr(i < len) {
-            return std::get<i>(tuple).size() + count<T, i + 1, len>(tuple);
+            return count<T, i + 1, len>(tuple, initial_size + std::get<i>(tuple).size());
         } else {
-            return 0;
+            return initial_size;
         }
     }
 
     template<typename T, size_t tuple_i, size_t tuple_len>
-    inline std::variant<Arguments...> get_item(const T &tuple, size_t i) const {
+    REMOVE_RECURSION inline std::variant<Arguments...> get_item(const T &tuple, size_t i) const {
         if constexpr(tuple_i < tuple_len) {
             if (i < std::get<tuple_i>(tuple).size()) {
                 return std::get<tuple_i>(tuple)[i];
@@ -31,6 +40,15 @@ private:
             }
         } else {
             Q_UNREACHABLE();
+        }
+    }
+
+    template<typename T, size_t i, size_t len>
+    REMOVE_RECURSION inline bool everything_empty(const T &tuple, bool initial_state = true) const {
+        if constexpr(i < len) {
+            return everything_empty<T, i + 1, len>(tuple, initial_state && std::get<i>(tuple).empty());
+        } else {
+            return initial_state;
         }
     }
 public:
@@ -47,6 +65,11 @@ public:
     std::variant<Arguments...> operator[](size_t i) const {
         constexpr size_t nvecs = std::tuple_size_v<decltype(m_vectors)>;
         return get_item<decltype(m_vectors), 0, nvecs>(m_vectors, i);
+    }
+
+    bool empty() const {
+        constexpr size_t nvecs = std::tuple_size_v<decltype(m_vectors)>;
+        return everything_empty<decltype(m_vectors), 0, nvecs>(m_vectors);
     }
 private:
     std::tuple<std::vector<Arguments>...> m_vectors;
