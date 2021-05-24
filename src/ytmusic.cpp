d861b7cc59c8eb37b844bedb7e3d100f6a1bc24d
Jonah Brüchert <jbb@kaidan.im>
2021-05-25 01:13:38 +0200

    Update tested ytmusicapi version

--- src/ytmusic.cpp
+++ src/ytmusic.cpp
@@ -15,7 +15,7 @@ using namespace py::literals;
 
 #define UNEXPORT __attribute__ ((visibility("hidden")))
 
-constexpr auto TESTED_YTMUSICAPI_VERSION = "0.16.0";
+constexpr auto TESTED_YTMUSICAPI_VERSION = "0.17.1";
 
 struct UNEXPORT YTMusicPrivate {
     py::scoped_interpreter guard {};
