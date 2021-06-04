1fa0a3f4b28d6c32471d3d0638e32bb93dba5e82
Jonah Brüchert <jbb@kaidan.im>
2021-06-04 17:22:29 +0200

    Add reuse headers to currently unlicensed files

--- flatpak/regenerate-sources.sh
+++ flatpak/regenerate-sources.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# SPDX-FileCopyrightText: 2021 Jonah Brüchert <jbb@kaidan.im>
+#
+# SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
 set -e
 
 export GIT_CLONE_ARGS="--depth 1 --single-branch"
