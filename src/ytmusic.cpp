db35f2a5e6a286f059dd30ddcec8640b18a98154
Jonah Brüchert <jbb@kaidan.im>
2021-07-12 00:24:55 +0200

    Update ytmusicapi

--- src/ytmusic.cpp
+++ src/ytmusic.cpp
@@ -15,7 +15,7 @@ using namespace py::literals;
 
 #define UNEXPORT __attribute__ ((visibility("hidden")))
 
-constexpr auto TESTED_YTMUSICAPI_VERSION = "0.17.3";
+constexpr auto TESTED_YTMUSICAPI_VERSION = "0.18.0";
 
 struct UNEXPORT YTMusicPrivate {
     py::scoped_interpreter guard {};
