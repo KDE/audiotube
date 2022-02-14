1d25f46f52ed787e2bf1d19f354066085c90d491
Jonah Brüchert <jbb@kaidan.im>
2022-02-14 01:03:48 +0100

    MultiIterableView: Try to make code easier to understand

--- src/multiiterableview.h
+++ src/multiiterableview.h
@@ -9,48 +9,27 @@
 #include <vector>
 #include <QtGlobal>
 
-// MultiIterableView is usually used on a small number of vectors.
-// The number is already known, so we can convert this into an if else if else etc tree
-// instead of recursively calling the function
-#ifdef __GNUC__
-#define REMOVE_RECURSION __attribute__((always_inline))
-#else
-#define REMOVE_RECURSION
-#endif
+template <typename Tuple, typename Func, std::size_t i>
+inline void iterate_impl(Tuple &tup, Func fun)
+{
+    if constexpr(i >= std::tuple_size_v<std::decay_t<decltype(tup)>>) {
+        return;
+    } else {
+        fun(std::get<i>(tup));
+        return iterate_impl<Tuple, Func, i + 1>(tup, fun);
+    }
+}
+
+template <typename Tuple, typename Func>
+inline void iterate_tuple(Tuple &tup, Func fun)
+{
+    iterate_impl<Tuple, Func, 0>(tup, fun);
+}
+
 
 template <typename... Arguments>
 class MultiIterableView {
 private:
-    template<typename T, size_t i, size_t len>
-    REMOVE_RECURSION inline size_t count(const T &tuple, int initial_size = 0) const {
-        if constexpr(i < len) {
-            return count<T, i + 1, len>(tuple, initial_size + std::get<i>(tuple).size());
-        } else {
-            return initial_size;
-        }
-    }
-
-    template<typename T, size_t tuple_i, size_t tuple_len>
-    REMOVE_RECURSION inline std::variant<Arguments...> get_item(const T &tuple, size_t i) const {
-        if constexpr(tuple_i < tuple_len) {
-            if (i < std::get<tuple_i>(tuple).size()) {
-                return std::get<tuple_i>(tuple)[i];
-            } else {
-                return get_item<T, tuple_i + 1, tuple_len>(tuple, i - std::get<tuple_i>(tuple).size());
-            }
-        } else {
-            Q_UNREACHABLE();
-        }
-    }
-
-    template<typename T, size_t i, size_t len>
-    REMOVE_RECURSION inline bool everything_empty(const T &tuple, bool initial_state = true) const {
-        if constexpr(i < len) {
-            return everything_empty<T, i + 1, len>(tuple, initial_state && std::get<i>(tuple).empty());
-        } else {
-            return initial_state;
-        }
-    }
 public:
     explicit MultiIterableView(std::vector<Arguments> &&...lists)
         : m_vectors(std::make_tuple(lists...))
@@ -58,18 +37,31 @@ public:
     }
 
     size_t size() const {
-        constexpr size_t nvecs = std::tuple_size_v<decltype(m_vectors)>;
-        return count<decltype(m_vectors), 0, nvecs>(m_vectors);
+        size_t s = 0;
+        iterate_tuple(m_vectors, [&](auto &vec) {
+            s += vec.size();
+        });
+        return s;
     }
 
     std::variant<Arguments...> operator[](size_t i) const {
-        constexpr size_t nvecs = std::tuple_size_v<decltype(m_vectors)>;
-        return get_item<decltype(m_vectors), 0, nvecs>(m_vectors, i);
+        size_t s = 0;
+        std::variant<Arguments...> out;
+        iterate_tuple(m_vectors, [&](auto &vec) {
+            if (i >= s && i < s + vec.size()) {
+                out = vec[i - s];
+            }
+            s += vec.size();
+        });
+        return out;
     }
 
     bool empty() const {
-        constexpr size_t nvecs = std::tuple_size_v<decltype(m_vectors)>;
-        return everything_empty<decltype(m_vectors), 0, nvecs>(m_vectors);
+        bool empty = true;
+        iterate_tuple(m_vectors, [&](auto &elem) {
+            empty &= elem.empty();
+        });
+        return empty;
     }
 private:
     std::tuple<std::vector<Arguments>...> m_vectors;
