51b64552ea64bee751ffd70b27b9dca308a5aaf1
Jonah Brüchert <jbb@kaidan.im>
2026-09-22 23:33:20 +0200

    Fix reuse compliance

--- src/migrations/2026-09-22-211215-0000_ids/down.sql
+++ src/migrations/2026-09-22-211215-0000_ids/down.sql
@@ -1,3 +1,6 @@
--- This file should undo anything in `up.sql`
+-- SPDX-FileCopyrightText: 2026 Jonah Brüchert <jbb@kaidan.im>
+--
+-- SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
 alter table songs drop column artist_id;
 alter table songs drop column album_id;
--- src/migrations/2026-09-22-211215-0000_ids/up.sql
+++ src/migrations/2026-09-22-211215-0000_ids/up.sql
@@ -1,3 +1,6 @@
--- Your SQL goes here
+-- SPDX-FileCopyrightText: 2026 Jonah Brüchert <jbb@kaidan.im>
+--
+-- SPDX-License-Identifier: GPL-2.0-only OR GPL-3.0-only OR LicenseRef-KDE-Accepted-GPL
+
 alter table songs add column artist_id Text;
 alter table songs add column album_id Text;
